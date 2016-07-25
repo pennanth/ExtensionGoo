e871c66f1e047265e5917734b06a919b7af11b28
Jordan Knight <jakkaj@gmail.com>
2016-07-25 13:37:54 -0700

    Added simple test to json test enpoint

---
+++ src/ExtensionGoo.Tests/Tests/SampleJsonEndpointTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using ExtensionGoo.Portable.Extensions;
+using ExtensionGoo.Tests.Entity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtensionGoo.Tests.Tests
+{
+    [TestClass]
+    public class SampleJsonEndpointTest
+    {
+        [TestMethod]
+        public async Task TestGetAndParse()
+        {
+            var url = "http://jsonplaceholder.typicode.com/posts/1";
+
+            var obj = await url.GetAndParse<TestEntity>();
+
+            Assert.IsNotNull(obj);
+
+            Assert.AreEqual(1, obj.userId);
+        }
+    }
+}
